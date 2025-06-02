8086b7ba5e09b949925b5b1701cca163756dbdb5
Ahmed Taha EL IDRISSI <161514845+atahael@users.noreply.github.com>
2025-06-02 20:25:54 +0100

    projet exam de c#

--- obj/Release/net8.0-windows/tp_examc#.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/tp_examc#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp_examc#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4515eeaad53fc6c9b590c482a74156e689a09a41")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp_examc#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp_examc#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
